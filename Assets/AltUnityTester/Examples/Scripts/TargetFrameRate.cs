4abbfb05767e607e39674581eee6a147c3ea1c0e
dorinoltean <dorinoltean@gmail.com>
2021-02-24 15:46:19 +0200

    added target framerate

---
+++ Assets/AltUnityTester/Examples/Scripts/TargetFrameRate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TargetFrameRate : MonoBehaviour
+{
+    public int targetFrameRate = 15;
+
+    protected void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    protected void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFrameRate;
+    }
+
+}

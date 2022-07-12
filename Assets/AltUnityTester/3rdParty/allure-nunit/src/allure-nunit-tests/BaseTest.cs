d4bca3051c8a3121c060e26dd04ba55e69471a3b
Gombos Kriszta <kriszta.gombos@altom.com>
2022-07-12 14:19:43 +0300

    added allure nunit package

---
+++ Assets/AltUnityTester/3rdParty/allure-nunit/src/allure-nunit-tests/BaseTest.cs
@@ -0,0 +1,19 @@
+﻿using Allure.Commons;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+
+namespace allure_nunit_tests
+{
+    [AllureNUnit]
+    [AllureParentSuite("Root Suite")]
+    public class BaseTest
+    {
+        [OneTimeSetUp]
+        public void CleanupResultDirectory()
+        {
+            AllureExtensions.WrapSetUpTearDownParams(() => { AllureLifecycle.Instance.CleanupResultDirectory(); },
+                "Clear Allure Results Directory");
+        }
+    }
+}
